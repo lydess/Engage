755d532ffe63dc6f32303cd60e1c57c594cc5d67
william vise <lightbekon@gmail.com>
2021-04-17 18:36:30 +1000

    Biggest working version
    last commit before final destructive testing

--- Engage/Viewcontrollers/debugcontroller.swift
+++ Engage/Viewcontrollers/debugcontroller.swift
@@ -11,6 +11,7 @@ import Foundation
 import UIKit
 import SceneKit
 
+
 class debugcontroller: UIViewController {
     
     @IBOutlet weak var toplabel: UILabel!
@@ -24,44 +25,56 @@ class debugcontroller: UIViewController {
     @IBOutlet weak var testview: UIStackView!
     
     
+    @IBOutlet var swiperight: UISwipeGestureRecognizer!
+    
     var activebut = [UIStackView]()
     var tboxpool = [UITextField]()
     
+    
     override func viewDidLoad() {
         topstack.spacing = 30.0
+        var keyboardType: UIKeyboardType = .numberPad
+        
     }
     
     @IBAction func debug1push(_ sender: Any) {
-        for x in 0...23{
-            buttongen(text: "button" + String(x),id: x)
+        for x in 0...19{
+            buttongen2(text: "button" + String(x),id: x)
         }
-        
+        buttongen(text: "Other", id: 20)
     }
     @IBAction func debug2push(_ sender: Any) {
-        buttongen2()
+        
     }
     
     @IBAction func debug3push(_ sender: Any) {
         for x in activebut {
         print(x)
         }
+        
+        
+        
+    }
+    @IBAction func swiperight(_ sender: Any) {
+        for x in tboxpool{
+            x.endEditing(true)
+        }
     }
     func buttongen(text: String, id: Int) {
         let tbox = UITextField()
         let butt = checkbox()
-       
         
+        tbox.returnKeyType = .done
         butt.frame = CGRect(x: 0, y: 0, width: 30, height: 60)
-        tbox.frame = CGRect(x: 0, y: 0, width: 30, height: 30)
         butt.setImage(UIImage.init(named: "unticked"), for: .normal)
-        tbox.text = text
-        tbox.textColor = .black
         butt.addTarget(self, action: #selector(click), for: .touchUpInside)
-        tbox.tag = id
         butt.tag = id
         
-        
-        
+        tbox.frame = CGRect(x: 0, y: 0, width: 30, height: 30)
+        tbox.text = text
+        tbox.textColor = .black
+        tbox.tag = id
+      
         var stackView   = UIStackView()
        	stackView = UIStackView(arrangedSubviews: [butt, tbox])
         stackView.alignment = .center
@@ -83,20 +96,41 @@ class debugcontroller: UIViewController {
             print("max buttons reached")
         }
         
-        
-//        stackView.centerXAnchor.constraint(equalTo: self.topstack.centerXAnchor).isActive = true
-//        stackView.centerYAnchor.constraint(equalTo: self.topstack.centerYAnchor).isActive = true
     }
-    func buttongen2() {
-        func buttongen(text: String) -> checkbox {
-            let pic = UIImage.init(named: "unticked")
-            let button = checkbox(frame: CGRect(x: 100, y:100, width: 100, height: 20))
-            button.backgroundColor = .black
-            button.setTitleColor(.black, for: .normal)
-            button.setImage(pic, for: .normal)
-            button.setTitle("  " + text, for: .normal)
-            topstack.addArrangedSubview(button)
-            return button
+    func buttongen2(text: String, id: Int) {
+        let tbox = UILabel()
+        let butt = checkbox()
+       
+        
+        butt.frame = CGRect(x: 0, y: 0, width: 30, height: 60)
+        butt.setImage(UIImage.init(named: "unticked"), for: .normal)
+        butt.addTarget(self, action: #selector(click), for: .touchUpInside)
+        butt.tag = id
+        
+        tbox.frame = CGRect(x: 0, y: 0, width: 30, height: 30)
+        tbox.text = text
+        tbox.textColor = .black
+        tbox.tag = id
+      
+        var stackView   = UIStackView()
+        stackView = UIStackView(arrangedSubviews: [butt, tbox])
+        stackView.alignment = .center
+        stackView.axis = .horizontal
+        stackView.distribution = .equalSpacing
+        stackView.spacing = 30.0
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        
+        activebut.append(stackView)
+        
+        
+        if topstack.arrangedSubviews.count < 12{
+            topstack.addArrangedSubview(stackView)
+        }
+        else if rightstack.arrangedSubviews.count < 12{
+            rightstack.addArrangedSubview(stackView)
+        }
+        else if activebut.count >= 25 {
+            print("max buttons reached")
         }
        
     }
@@ -122,6 +156,7 @@ class debugcontroller: UIViewController {
         var clickedid = sender.tag
         for x in tboxpool {
             if x.tag == clickedid{
+                print("button clicked was " + x.text!)
                 
             }
         }
