089ee396de091532f3d3dbf68af0e3bada3df7d4
william vise <lightbekon@gmail.com>
2021-05-01 08:27:34 +1000

    reboot commit

--- Engage/Viewcontrollers/courseinfocontroller.swift
+++ Engage/Viewcontrollers/courseinfocontroller.swift
@@ -9,8 +9,16 @@ import Foundation
 import  UIKit
 
 class courseinfoviewcontroller: UIViewController {
+    @IBOutlet weak var topbar: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         print("welcome to course info")
+        initalize()
+    }
+    
+    func initalize() {
+        topbar.layer.shadowOffset = .init(width: 500, height: 500)
+        
     }
 }
---
+++ Engage/Viewcontrollers/pageviewcontroller.swift
@@ -0,0 +1,34 @@
+//
+//  File.swift
+//  Engage
+//
+//  Created by william Vise on 30/4/21.
+//
+
+import Foundation
+import  UIKit
+
+class pageviewcontroller: UIPageViewController {
+    @IBOutlet weak var topbar: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("welcome to course info")
+        initalize()
+    }
+    
+    func initalize() {
+        var datasource = UIPageViewControllerDataSource.self
+        let viewlist = [UIViewController]()
+        let firstview = UIViewController()
+        
+        
+        
+        firstview.view.backgroundColor = .yellow
+        firstview.view.frame = CGRect(x: 200, y: 200, width: 200, height: 200)
+        
+        self.setViewControllers(viewlist, direction: .forward, animated: true, completion: {_ in print("viewsadded")})
+       
+        
+    }
+}
