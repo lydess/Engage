46817fa50bc335f754851db1b208bf777b5ef743
william vise <lightbekon@gmail.com>
2021-05-05 14:00:37 +1000

    Commit before changing newsfeed
    Deleting shit

---
+++ Engage/Viewcontrollers/Newsfeedcontroller.swift
@@ -0,0 +1,14 @@
+//
+//  Newsfeedcontroller.swift
+//  Engage
+//
+//  Created by william Vise on 5/5/21.
+//
+
+import Foundation
+import UIKit
+class newsfeedcontroller: UIViewController {
+    override func viewDidLoad() {
+        print("news")
+    }
+}
